a3f670bdcb83b91907d528042eb8c35013c1b9f0
udragan <ujfa2001@yahoo.com>
2018-06-23 21:06:54 +0200

    added Notes.Model project and User and Note classes.

---
+++ solution/WebApi.Notes/Notes.Model/DbModels/Note.cs
@@ -0,0 +1,11 @@
+﻿namespace com.udragan.netCore.webApi.Notes.Model.DbModels
+{
+	public class Note
+	{
+		public long Id { get; set; }
+
+		public long UserId { get; set; }
+		public User User { get; set; }
+		public string Content { get; set; }
+	}
+}
---
+++ solution/WebApi.Notes/Notes.Model/DbModels/User.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace com.udragan.netCore.webApi.Notes.Model.DbModels
+{
+	public class User
+	{
+		public long Id { get; set; }
+
+		public string Name { get; set; }
+		public IList<Note> Notes { get; set; }
+	}
+}
