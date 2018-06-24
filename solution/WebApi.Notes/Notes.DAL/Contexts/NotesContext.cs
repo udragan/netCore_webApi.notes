6266426cfbc70236fa36c6b9403121249e74c7c4
udragan <ujfa2001@yahoo.com>
2018-06-24 11:08:55 +0200

    added Notes.DAL project.

---
+++ solution/WebApi.Notes/Notes.DAL/Contexts/NotesContext.cs
@@ -0,0 +1,15 @@
+﻿using com.udragan.netCore.webApi.Notes.Model.DbModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace com.udragan.netCore.webApi.Notes.DAL.Contexts
+{
+	public class NotesContext : DbContext
+	{
+		public NotesContext(DbContextOptions<NotesContext> options)
+			: base(options)
+		{ }
+
+		public DbSet<User> Users { get; set; }
+		public DbSet<Note> Notes { get; set; }
+	}
+}
