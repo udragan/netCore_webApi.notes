4cca4ab5b9742752aa4d6f96f6191093e135de61
udragan <ujfa2001@yahoo.com>
2018-06-27 19:52:15 +0200

    added test data seed.

--- solution/WebApi.Notes/Notes/Startup.cs
+++ solution/WebApi.Notes/Notes/Startup.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Collections.Generic;
+using com.udragan.netCore.webApi.Notes.DAL.Contexts;
+using com.udragan.netCore.webApi.Notes.Model.DbModels;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -17,6 +21,7 @@ namespace com.udragan.netCore.webApi.Notes
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddDbContext<NotesContext>(opt => opt.UseInMemoryDatabase(databaseName: "inMemoryDb"));
 			services.AddMvc();
 		}
 
@@ -25,10 +30,50 @@ namespace com.udragan.netCore.webApi.Notes
 		{
 			if (env.IsDevelopment())
 			{
+				using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+				{
+					NotesContext context = serviceScope.ServiceProvider.GetRequiredService<NotesContext>();
+
+					SeedTestData(context);
+				}
+
 				app.UseDeveloperExceptionPage();
 			}
 
 			app.UseMvc();
 		}
+
+		#region Private methods
+
+		private static void SeedTestData(NotesContext context)
+		{
+			context.AddRange(new User
+			{
+				Name = "TestUser1",
+				Notes = new List<Note>
+				{
+					new Note
+					{
+						Content = "First content.",
+					},
+					new Note
+					{
+						Content = "Second note content of TestUser1.",
+					},
+				},
+			},
+			new User
+			{
+				Name = "TestUser2",
+			},
+			new User
+			{
+				Name = "TestUser3",
+			});
+
+			context.SaveChanges();
+		}
+
+		#endregion
 	}
 }
