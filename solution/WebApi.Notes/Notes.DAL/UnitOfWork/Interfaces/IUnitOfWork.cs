39d64c09ff439f244c9255d46e033bb829627471
udragan <ujfa2001@yahoo.com>
2018-07-17 19:40:59 +0200

    added generic UnitOfWork interface.

---
+++ solution/WebApi.Notes/Notes.DAL/UnitOfWork/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace com.udragan.netCore.webApi.Notes.DAL.UnitOfWork.Interfaces
+{
+	/// <summary>
+	/// Generic Unit Of Work interface.
+	/// </summary>
+	/// <seealso cref="System.IDisposable" />
+	interface IUnitOfWork : IDisposable
+	{
+		/// <summary>
+		/// Gets the context.
+		/// </summary>
+		DbContext Context { get; }
+
+		/// <summary>
+		/// Commits the changes to the context.
+		/// </summary>
+		void Commit();
+	}
+}
---
+++ solution/WebApi.Notes/Notes.DAL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,58 @@
+﻿using com.udragan.netCore.webApi.Notes.DAL.UnitOfWork.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace com.udragan.netCore.webApi.Notes.DAL.UnitOfWork
+{
+	/// <summary>
+	/// UnitOfWork implementation.
+	/// </summary>
+	/// <seealso cref="com.udragan.netCore.webApi.Notes.DAL.UnitOfWork.Interfaces.IUnitOfWork" />
+	class UnitOfWork : IUnitOfWork
+	{
+		#region Fields
+
+		/// <summary>
+		/// Gets the context.
+		/// </summary>
+		public DbContext Context { get; }
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="UnitOfWork"/> class.
+		/// </summary>
+		/// <param name="context">The context.</param>
+		public UnitOfWork(DbContext context)
+		{
+			Context = context;
+		}
+
+		#endregion
+
+		#region IUnitOfWork
+
+		/// <summary>
+		/// Commits the changes to the context.
+		/// </summary>
+		public void Commit()
+		{
+			Context.SaveChangesAsync();
+		}
+
+		#endregion
+
+		#region IDisposable
+
+		/// <summary>
+		/// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+		/// </summary>
+		public void Dispose()
+		{
+			Context.Dispose();
+		}
+
+		#endregion
+	}
+}
