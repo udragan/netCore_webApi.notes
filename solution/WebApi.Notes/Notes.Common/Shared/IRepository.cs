79ea577ba69f80c8eeb54afe80ba22f3a87985c3
udragan <ujfa2001@yahoo.com>
2018-09-04 18:42:43 +0200

    added base Entity, IRepository and IUnitOfWork to Common.

---
+++ solution/WebApi.Notes/Notes.Common/Shared/IRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace com.udragan.netCore.webApi.Notes.Common.Shared
+{
+	/// <summary>
+	/// Generic repository interface.
+	/// </summary>
+	/// <typeparam name="TEntity">The entity type.</typeparam>
+	public interface IRepository<TEntity>
+		where TEntity : Entity
+	{
+		/// <summary>
+		/// Gets all entities.
+		/// </summary>
+		/// <returns>
+		/// A collection of all available entities in the repository.
+		/// </returns>
+		IEnumerable<TEntity> GetAll();
+
+		/// <summary>
+		/// Gets the entity by its identifier.
+		/// </summary>
+		/// <param name="id">The identifier.</param>
+		/// <returns>
+		/// The entity with provided identifier.
+		/// </returns>
+		TEntity Get(long id);
+
+		/// <summary>
+		/// Finds the specified predicate.
+		/// </summary>
+		/// <param name="predicate">The predicate.</param>
+		/// <returns>
+		/// A collection of entities that satisfy the predicate.
+		/// </returns>
+		IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+
+		/// <summary>
+		/// Adds the specified entity to the repository.
+		/// </summary>
+		/// <param name="entity">The entity to create.</param>
+		void Add(TEntity entity);
+
+		/// <summary>
+		/// Removes the entity from reporitory.
+		/// </summary>
+		/// <param name="entity">The entity.</param>
+		void Remove(TEntity entity);
+	}
+}
---
+++ solution/WebApi.Notes/Notes.Common/Shared/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace com.udragan.netCore.webApi.Notes.Common.Shared
+{
+	/// <summary>
+	/// Generic Unit Of Work interface.
+	/// </summary>
+	/// <seealso cref="System.IDisposable" />
+	public interface IUnitOfWork : IDisposable
+	{
+		/// <summary>
+		/// Commits the changes to the context.
+		/// </summary>
+		void Commit();
+	}
+}
