9e63a041665c7b8b5ed7582b4576411c8a9a5a49
udragan <ujfa2001@yahoo.com>
2018-09-06 19:17:50 +0200

    added Note and User domain models.

---
+++ solution/WebApi.Notes/Notes.Domain/Models/Note.cs
@@ -0,0 +1,62 @@
+﻿using com.udragan.netCore.webApi.Notes.Common.Shared;
+
+namespace com.udragan.netCore.webApi.Notes.Domain.Models
+{
+	/// <summary>
+	/// POCO class representing the note.
+	/// </summary>
+	/// <seealso cref="com.udragan.netCore.webApi.Notes.Common.Shared.Entity" />
+	public class Note : Entity
+	{
+		#region Members
+
+		private long _userId;
+		private string _caption;
+		private string _text;
+		private bool _isChecked;
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Note"/> class.
+		/// </summary>
+		/// <param name="userId">The user identifier.</param>
+		/// <param name="caption">The caption.</param>
+		/// <param name="text">The text.</param>
+		/// <param name="isChecked">if set to <c>true</c> [is checked].</param>
+		public Note(
+			long userId,
+			string caption,
+			string text,
+			bool isChecked)
+		{
+			_userId = userId;
+			_caption = caption;
+			_text = text;
+			_isChecked = isChecked;
+		}
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Gets the note caption.
+		/// </summary>
+		public string Caption => _caption;
+
+		/// <summary>
+		/// Gets the note text.
+		/// </summary>
+		public string Text => _text;
+
+		/// <summary>
+		/// Gets a value indicating whether the note is checked.
+		/// </summary>
+		public bool IsChecked => _isChecked;
+
+		#endregion
+	}
+}
---
+++ solution/WebApi.Notes/Notes.Domain/Models/User.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using com.udragan.netCore.webApi.Notes.Common.Shared;
+
+namespace com.udragan.netCore.webApi.Notes.Domain.Models
+{
+	/// <summary>
+	/// POCO class representing the user.
+	/// </summary>
+	/// <seealso cref="com.udragan.netCore.webApi.Notes.Common.Shared.Entity" />
+	public class User : Entity
+	{
+		#region Members
+
+		private string _name;
+		private List<Note> _notes;
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="User"/> class.
+		/// </summary>
+		/// <param name="name">The name.</param>
+		public User(string name)
+		{
+			_name = name;
+			_notes = new List<Note>();
+		}
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Gets the user`s name.
+		/// </summary>
+		public string Name => _name;
+
+		/// <summary>
+		/// Gets the user`s notes.
+		/// </summary>
+		public IReadOnlyCollection<Note> Notes => _notes;
+
+		#endregion
+
+		#region Public methods
+
+		/// <summary>
+		/// Adds the note.
+		/// </summary>
+		/// <param name="caption">The note caption.</param>
+		/// <param name="text">The note text.</param>
+		/// <param name="isChecked">The note state.</param>
+		public void AddNote(string caption, string text, bool isChecked)
+		{
+			Note note = new Note(Id, caption, text, isChecked);
+
+			_notes.Add(note);
+		}
+
+		/// <summary>
+		/// Remotes the note.
+		/// </summary>
+		/// <param name="noteId">The note identifier.</param>
+		public void RemoteNote(long noteId)
+		{
+			_notes.RemoveAll(x => x.Id == noteId);
+		}
+
+		#endregion
+	}
+}
