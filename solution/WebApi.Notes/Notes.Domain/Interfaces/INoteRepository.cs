95395c834eadcebca2700eff0a2e6b94ec95d24b
udragan <ujfa2001@yahoo.com>
2018-09-12 19:16:40 +0200

    added INoteRepository to Domain.

---
+++ solution/WebApi.Notes/Notes.Domain/Interfaces/INoteRepository.cs
@@ -0,0 +1,12 @@
+﻿using com.udragan.netCore.webApi.Notes.Common.Shared;
+using com.udragan.netCore.webApi.Notes.Domain.Models;
+
+namespace com.udragan.netCore.webApi.Notes.Domain.Interfaces
+{
+	/// <summary>
+	/// Repository interface for notes.
+	/// </summary>
+	/// <seealso cref="com.udragan.netCore.webApi.Notes.Common.Shared.IRepository{com.udragan.netCore.webApi.Notes.Domain.Models.Note}" />
+	public interface INoteRepository : IRepository<Note>
+	{ }
+}
