dc4d6847aacba92e9f2f7c999be86e973399a2d4
udragan <ujfa2001@yahoo.com>
2018-09-19 17:49:42 +0200

    added IUserRepository to Domain.

---
+++ solution/WebApi.Notes/Notes.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using com.udragan.netCore.webApi.Notes.Common.Shared;
+using com.udragan.netCore.webApi.Notes.Domain.Models;
+
+namespace com.udragan.netCore.webApi.Notes.Domain.Interfaces
+{
+	/// <summary>
+	/// Repository interface for users.
+	/// </summary>
+	/// <seealso cref="com.udragan.netCore.webApi.Notes.Common.Shared.IRepository{com.udragan.netCore.webApi.Notes.Domain.Models.User}" />
+	public interface IUserRepository : IRepository<User>
+	{ }
+}
